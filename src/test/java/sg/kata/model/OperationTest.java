9045249dccb6472a66229ce1be8fb01ea3b5a3f6
Romain <romaingraziani@gmail.com>
2022-04-05 19:44:30 +0200

    test: augmentation couverture (~ 100%)

--- src/test/java/sg/kata/model/OperationTest.java
+++ src/test/java/sg/kata/model/OperationTest.java
@@ -1,14 +1,29 @@
 package sg.kata.model;
 
-        import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 
-        import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
+
+import static java.math.BigDecimal.TEN;
+import static org.fest.assertions.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static sg.kata.model.OperationType.WITHHDRAW;
 
 class OperationTest {
 
     @Test
     void on_ne_peut_pas_creer_d_operation_incomplete() {
         assertThrows(Exception.class, () -> new Operation(null, null, null));
+    }
+
+
+    @Test
+    void equivalence_hascode_et_equals() {
+        Operation o1 = new Operation(WITHHDRAW, LocalDate.of(2022, 2, 3), TEN);
+        Operation o2 = new Operation(WITHHDRAW, LocalDate.of(2022, 2, 3), TEN);
+        assertThat(o1).isNotSameAs(o2);
+        assertThat(o1.equals(o2) && o2.equals(o1)).isTrue();
+        assertThat(o1.hashCode() == o2.hashCode()).isTrue();
     }
 
 }
