00257730985b084cb0d5a726112c39034db97137
Romain <romaingraziani@gmail.com>
2022-04-05 15:03:38 +0200

    feat(bank-account): creation - withdraw - deposit

---
+++ src/main/java/sg/kata/Kata.java
@@ -0,0 +1,10 @@
+package sg.kata;
+
+public class Kata {
+
+
+
+    public static void main(String...args) {
+    }
+
+}
---
+++ src/main/java/sg/kata/model/OperationType.java
@@ -0,0 +1,6 @@
+package sg.kata.model;
+
+public enum OperationType {
+    WITHHDRAW,
+    DEPOSIT
+}
